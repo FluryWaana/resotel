0ef37b0b704b9da8bacb2410d6fba0dcb999e404
Axel PICOULET <contact@apicoulet.com>
2019-09-11 21:25:47 +0200

    Add Class RelayCommand, ViewModelBase

---
+++ Resotel/ViewsModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Resotel.ViewsModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string str = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(str));
+        }
+    }
+}
