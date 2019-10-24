5e9dab794708d06cfae5dfcc0d4ce0d113e06af2
clemence dodeler <clemence.dodeler@gmail.com>
2019-10-24 18:45:18 +0200

    Commit 1 klem

---
+++ 02-Application/Repositories/BookingRepository.cs
@@ -0,0 +1,45 @@
+﻿using Resotel.Entities;
+using Resotel.Shared;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Resotel.Repositories
+{
+    class BookingRepository : BaseRepository
+    {
+
+        /**
+        * Constructeur
+        */
+        public BookingRepository()
+        {
+        }
+
+        //--------------------------------------------------------------------
+
+        public booking addBooking()
+        {
+            booking book = new booking();
+
+            // ici ajouter des fake reservations pour test
+            book.bedroom_number = 10;
+            
+
+
+            var query = entities.booking.Add(book);
+
+            return null;
+
+        }
+
+
+        public List<booking> GetBookings()
+        {
+            // Récupère toutes les reservations
+            var query = entities.booking.Select(x => x);
+
+            return query.ToList();
+        }
+    }
+}
--- 02-Application/ViewsModels/BookingViewModel.cs
+++ 02-Application/ViewsModels/BookingViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Resotel.Entities;
+using Resotel.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace Resotel.ViewsModels
 {
     public class BookingViewModel : ViewModelBase
     {
+        /**
+         * Liste des reservations
+         */
+        private List<booking> bookings;
+
+        private BookingRepository bookingRepository;
+
+        /*
+         * Constructeur
+         */
+        public BookingViewModel()
+        {
+            bookingRepository = new BookingRepository();
+            Bookings = bookingRepository.GetBookings();
+        }
+
+        public List<booking> Bookings
+        {
+            get
+            {
+                return bookings;
+            }
+
+            set
+            {
+                bookings = value;
+                NotifyPropertyChanged("Bookings");
+            }
+        }
     }
 }
