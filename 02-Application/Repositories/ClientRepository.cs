71650208b813abcf038d438d6e3337758c84398f
Axel PICOULET <contact@apicoulet.com>
2019-10-26 15:37:11 +0200

    - Ajout branche Qchiant

--- 02-Application/Repositories/ClientRepository.cs
+++ 02-Application/Repositories/ClientRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Resotel.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,53 @@ namespace Resotel.Repositories
 {
     public class ClientRepository : BaseRepository
     {
+        /**
+        * Constructeur
+        */
+        public ClientRepository()
+        {
+        }
+
+        //--------------------------------------------------------------------
+
+        /**
+         * 
+         */
+        public client addClient()
+        {
+            client cl = new client();
+            cl.client_lastname = "";
+            cl.client_firstname = "";
+
+            var query = entities.client.Add(cl);
+
+            return null;
+        }
+
+        //--------------------------------------------------------------------
+
+        /**
+         * Récupère tous les clients selons les filtres
+         */
+        public List<client> GetClients(string lastname = "", string firstname = "")
+        {
+            // Récupère tous les clients
+            var query = entities.client.Select(x => x);
+
+            // Si un lastname est mis dans les filtres de recherche
+            if (!lastname.Equals(""))
+            {
+                query = query.Where(x => x.client_lastname.Equals(lastname));
+
+            }
+
+            // Si firstname est mis dans les filtres de recherche
+            if (!firstname.Equals(""))
+            {
+                query = query.Where(x => x.client_lastname.Equals(firstname));
+            }
+
+            return query.ToList();
+        }
     }
 }
--- 02-Application/ViewsModels/ClientViewModel.cs
+++ 02-Application/ViewsModels/ClientViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Resotel.Entities;
+using System;
 using System.Collections.Generic;
+using Resotel.Repositories;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,37 @@ namespace Resotel.ViewsModels
 {
     public class ClientViewModel : ViewModelBase
     {
+        /**
+        * Liste des clients
+        */
+        private List<client> clients;
+
+        private ClientRepository clientRepository;
+
+        //--------------------------------------------------------------------
+
+        /*
+         * Constructeur
+         */
+        public ClientViewModel()
+        {
+            clientRepository = new ClientRepository();
+            Clients = clientRepository.GetClients();
+        }
+
+
+        public List<client> Clients
+        {
+            get
+            {
+                return clients;
+            }
+
+            set
+            {
+                clients = value;
+                NotifyPropertyChanged("Clients");
+            }
+        }
     }
 }
