4d901bbe23b18d8f28be7fdb244224e2dea1fba5
Axel PICOULET <contact@apicoulet.com>
2019-10-26 21:56:29 +0200

    Aide Quentin -> Liste Client

--- 02-Application/ViewsModels/ClientViewModel.cs
+++ 02-Application/ViewsModels/ClientViewModel.cs
@@ -1,20 +1,40 @@
 ﻿using Resotel.Entities;
+using Resotel.Repositories;
+using Resotel.Shared;
 using System;
 using System.Collections.Generic;
-using Resotel.Repositories;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace Resotel.ViewsModels
 {
     public class ClientViewModel : ViewModelBase
     {
         /**
-        * Liste des clients
-        */
-        private List<client> clients;
+         *  Collection de clients observés
+         */
+        private ObservableCollection<FormClientViewModel> listClient;
 
+        /**
+         * Client selectionné
+         */
+        private FormClientViewModel clientSelected;
+
+        /**
+         * 
+         */
+        private ICollectionView observer;
+
+        /**
+         * Liste des clients de la base de données
+         */
+        private List<client> clientsDataBase;
+
+        /**
+         * 
+         */
         private ClientRepository clientRepository;
 
         //--------------------------------------------------------------------
@@ -24,23 +44,95 @@ namespace Resotel.ViewsModels
          */
         public ClientViewModel()
         {
+            // Instanciation de ClientRepository
             clientRepository = new ClientRepository();
-            Clients = clientRepository.GetClients();
+
+            // Récupération des clients dans la base de données
+            clientsDataBase = clientRepository.GetClients();
+
+            // Instanciation de ObservableCollection
+            listClient = new ObservableCollection<FormClientViewModel>();
+
+            /**
+             * Remplissage de la collection d'observable avec les clients 
+             * de la base de données
+             */
+            foreach (client client in clientsDataBase)
+            {
+                addClientList(client);
+            }
+
+            observer = CollectionViewSource.GetDefaultView(listClient);
+            observer.CurrentChanged += Observer_CurrentChanged;
+            observer.MoveCurrentToFirst();
         }
 
+        //--------------------------------------------------------------------
 
-        public List<client> Clients
+        /**
+         * Liste des clients
+         */
+        public ObservableCollection<FormClientViewModel> ListClient
+        {
+            get => listClient;
+
+            set
+            {
+                if (value != listClient)
+                {
+                    listClient = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        /**
+         * Notifie que le client sélectionné a changé
+         */
+        private void Observer_CurrentChanged(object sender, EventArgs e)
+        {
+            NotifyPropertyChanged("ClientSelected");
+        }
+
+        /**
+         * Supprime le client selectionné
+         */
+        private void delete(object sender, EventArgs e)
+        {
+            this.listClient.Remove(ClientSelected);
+            NotifyPropertyChanged("ListClients");
+            observer.MoveCurrentToFirst();
+        }
+
+        /**
+         * Client selectionné
+         */
+        public FormClientViewModel ClientSelected
         {
             get
             {
-                return clients;
+                return (FormClientViewModel)observer.CurrentItem;
             }
 
             set
             {
-                clients = value;
-                NotifyPropertyChanged("Clients");
+                LogSystem.WriteLog("aaaaaa", TypeLog.Information);
+                clientSelected = value;
+                NotifyPropertyChanged();
             }
+        }
+
+        /**
+         * Ajoute un client
+         * Ajoute l'evenement de suppression au client
+         */
+        private void addClientList(client client)
+        {
+            FormClientViewModel temp = new FormClientViewModel(client);
+            temp.ClientEvent += delete;
+            listClient.Add(temp);
         }
     }
 }
---
+++ 02-Application/ViewsModels/FormClientViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Resotel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resotel.ViewsModels
+{
+    public delegate void ClientEventHandler(object sender, EventArgs e);
+    public class FormClientViewModel : ViewModelBase
+    {
+        /**
+         *  Client
+         */
+        private client client;
+
+        //-----------------------------------------------------------
+
+        /**
+         * Constructeur
+         */
+        public FormClientViewModel(client client)
+        {
+            this.client = client;
+        }
+
+        //--------------------------------------------------------------
+
+        public client Client
+        {
+            get => client;
+            set
+            {
+                client = value;
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        /**
+         *  
+         */
+        public ClientEventHandler ClientEvent;
+        public void RaiseTheEvent()
+        {
+            ClientEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
